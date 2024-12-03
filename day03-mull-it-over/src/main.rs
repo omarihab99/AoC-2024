b4c4d502376956f6dff184ba492a6766d0581ef1
omarihab99 <omar.ihab.mohamed99@gmail.com>
2024-12-03 19:02:23 +0200

    refactored for flexibility and finished day03 part02

--- day03-mull-it-over/src/main.rs
+++ day03-mull-it-over/src/main.rs
@@ -1,21 +1,38 @@
 use regex::Regex;
 fn main() {
     let input = read_input();
-    println!("{}", part_one(input.clone()));
+    println!("{}", solve(input.clone(), true)); //change to false for part_one of the puzzle.
 }
-fn part_one(input: String) -> u32 {
+fn solve(input: String, _2: bool) -> u32 {
     let mut sum: u32 = 0;
-    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut mul: bool = true;
+    let pat = match _2 {
+        true => r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)",
+        false => r"mul\((\d{1,3}),(\d{1,3})\)",
+    };
+    let re = Regex::new(pat).unwrap();
     re.find_iter(&input).for_each(|mat| {
-        sum += mat
-            .as_str()
-            .replace("mul(", "")
-            .replace(")", "")
-            .split(',')
-            .map(|x| x.parse::<u32>().unwrap())
-            .fold(1, |a, b| a * b);
+        let mat = mat.as_str();
+        if _2 {
+            if mat.starts_with("mul") {
+                if mul {
+                    sum += calc(mat);
+                }
+            } else {
+                mul = !mat.starts_with("don't")
+            }
+        } else {
+            sum += calc(mat);
+        }
     });
     sum
+}
+fn calc(mat: &str) -> u32 {
+    mat.replace("mul(", "")
+        .replace(")", "")
+        .split(',')
+        .map(|x| x.parse::<u32>().unwrap())
+        .fold(1, |a, b| a * b)
 }
 fn read_input() -> String {
     let input = include_str!("input.txt");
