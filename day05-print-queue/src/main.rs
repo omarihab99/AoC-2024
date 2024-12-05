85d665a6d185bb090a66892bdb4e2c6514279a42
omarihab99 <omar.ihab.mohamed99@gmail.com>
2024-12-06 00:56:44 +0200

    Solved day05 part02 with code refactoring for efficiency

--- day05-print-queue/src/main.rs
+++ day05-print-queue/src/main.rs
@@ -1,55 +1,81 @@
 use std::collections::HashMap;
-
 fn main() {
     let (rules, updates) = read_input();
-    println!("{}", solve(rules, updates));
+    let res = solve(rules, updates, true);
+    println!("{} ", res);
 }
-fn solve(rules: HashMap<u32, Vec<u32>>, updates: Vec<Vec<u32>>) -> u32 {
-    let mut middles: Vec<u32> = Vec::new();
-    updates.iter().for_each(|update| {
-        if is_valid_order(&rules, update) {
-            middles.push(update[update.len() / 2]);
-        }
-    });
-    middles.iter().sum()
+fn solve(rules: HashMap<u32, Vec<u32>>, updates: Vec<Vec<u32>>, _2: bool) -> u32 {
+    updates
+        .iter()
+        .filter_map(|update| {
+            let sorted = topological_sort(update, &rules);
+            let condition = if _2 {
+                &sorted != update
+            } else {
+                &sorted == update
+            };
+            if !sorted.is_empty() && condition {
+                Some(sorted[sorted.len() / 2])
+            } else {
+                None
+            }
+        })
+        .sum()
 }
-fn is_valid_order(rules: &HashMap<u32, Vec<u32>>, update: &Vec<u32>) -> bool {
-    for i in 0..update.len() {
-        for j in i + 1..update.len() {
-            if !rules.get(&update[i]).unwrap().contains(&update[j]) {
-                return false;
+fn topological_sort(update: &Vec<u32>, rules: &HashMap<u32, Vec<u32>>) -> Vec<u32> {
+    let mut in_degree: HashMap<u32, usize> = update.iter().map(|&u| (u, 0)).collect();
+    for (u, neighbors) in rules {
+        if update.contains(u) {
+            for v in neighbors {
+                if update.contains(v) {
+                    *in_degree.entry(*v).or_default() += 1;
+                }
             }
         }
     }
-    true
+    let mut queue: Vec<u32> = in_degree
+        .iter()
+        .filter_map(|(&k, &v)| if v == 0 { Some(k) } else { None })
+        .collect();
+    let mut ans = Vec::new();
+    while let Some(u) = queue.pop() {
+        ans.push(u);
+        if let Some(neighbors) = rules.get(&u) {
+            for &v in neighbors {
+                if update.contains(&v) {
+                    if let Some(degree) = in_degree.get_mut(&v) {
+                        *degree -= 1;
+                        if *degree == 0 {
+                            queue.push(v);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if ans.len() == update.len() {
+        ans
+    } else {
+        Vec::new()
+    }
 }
 fn read_input() -> (HashMap<u32, Vec<u32>>, Vec<Vec<u32>>) {
     let input = include_str!("input.txt");
-    let mut sections = input.split("\n\n");
-    let rules = sections
-        .next()
-        .unwrap()
+    let (rules_str, updates_str) = input.split_once("\n\n").unwrap();
+
+    let rules = rules_str.lines().fold(HashMap::new(), |mut acc, line| {
+        let mut parts = line.split('|').map(|s| s.parse::<u32>().unwrap());
+        let key = parts.next().unwrap();
+        let value = parts.next().unwrap();
+        acc.entry(key).or_insert_with(Vec::new).push(value);
+        acc
+    });
+
+    let updates = updates_str
         .lines()
-        .map(|line| {
-            let mut parts = line.split('|').map(|s| s.trim().parse::<u32>().unwrap());
-            let key = parts.next().unwrap();
-            let value = parts.next().unwrap();
-            (key, value)
-        })
-        .fold(HashMap::new(), |mut acc, (key, value)| {
-            acc.entry(key).or_insert_with(Vec::new).push(value);
-            acc
-        });
-    let updates = sections
-        .next()
-        .unwrap()
-        .lines()
-        .map(|line| {
-            line.split(',')
-                .map(|s| s.trim().parse::<u32>().unwrap())
-                .collect::<Vec<u32>>()
-        })
+        .map(|line| line.split(',').map(|s| s.parse::<u32>().unwrap()).collect())
         .collect();
+
     (rules, updates)
 }
 #[cfg(test)]
