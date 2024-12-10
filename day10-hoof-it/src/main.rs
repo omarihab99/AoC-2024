c5f5ed17398193e6ee519eeedbf546a9cd3bee1c
omarihab99 <omar.ihab.mohamed99@gmail.com>
2024-12-10 22:28:27 +0200

    Solved day10 part02

--- day10-hoof-it/src/main.rs
+++ day10-hoof-it/src/main.rs
@@ -1,18 +1,17 @@
 use std::collections::HashSet;
 
 fn main() {
-    println!("Hello, world!");
     let input = read_input();
-    println!("{}", solve(input));
+    println!("{}", solve(input, true));
 }
-fn solve(mut input: Vec<Vec<u32>>) -> u32 {
+fn solve(mut input: Vec<Vec<u32>>, _2: bool) -> u32 {
     let mut sum = 0;
     for i in 0..input.len() {
         for j in 0..input[0].len() {
             let mut val = 0;
             let mut visited = HashSet::new();
             if input[i][j] == 0 {
-                recurse(i, j, &mut input, &mut val, &mut visited);
+                recurse(i, j, &mut input, &mut val, &mut visited, _2);
                 sum += val;
             }
         }
@@ -25,23 +24,28 @@ fn recurse(
     input: &mut Vec<Vec<u32>>,
     val: &mut u32,
     visited: &mut HashSet<(usize, usize)>,
+    _2: bool,
 ) {
-    if !visited.contains(&(i, j)) && input[i][j] == 9 {
-        *val += 1;
-        visited.insert((i, j));
+    if input[i][j] == 9 {
+        if _2 {
+            *val += 1;
+        } else if !visited.contains(&(i, j)) {
+            visited.insert((i, j));
+            *val += 1;
+        }
         return;
     }
     if i > 0 && input[i - 1][j] == input[i][j] + 1 {
-        recurse(i - 1, j, input, val, visited);
+        recurse(i - 1, j, input, val, visited, _2);
     }
     if i < input.len() - 1 && input[i + 1][j] == input[i][j] + 1 {
-        recurse(i + 1, j, input, val, visited);
+        recurse(i + 1, j, input, val, visited, _2);
     }
     if j > 0 && input[i][j - 1] == input[i][j] + 1 {
-        recurse(i, j - 1, input, val, visited);
+        recurse(i, j - 1, input, val, visited, _2);
     }
     if j < input[0].len() - 1 && input[i][j + 1] == input[i][j] + 1 {
-        recurse(i, j + 1, input, val, visited);
+        recurse(i, j + 1, input, val, visited, _2);
     }
 }
 fn read_input() -> Vec<Vec<u32>> {
@@ -63,6 +67,6 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = read_input();
-        assert_eq!(solve(input), 36);
+        assert_eq!(solve(input, true), 81);
     }
 }
